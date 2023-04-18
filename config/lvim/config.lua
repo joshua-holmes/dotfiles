f15b912d56e14ae22647a6b373c1913202d2b68e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-04-18 10:29:23 -0700

    Change color scheme

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "tokyonight-night"
+lvim.colorscheme = "tokyonight-storm"
 lvim.transparent_window = true
 vim.opt.breakindent = true
 vim.opt.scrolloff = 999
