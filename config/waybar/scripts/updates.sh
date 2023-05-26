daaeb1bed451316a62026a3a9aba7d6f7257c915
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-05-25 19:51:50 -0700

    Change style and update delay for Waybar

--- config/waybar/scripts/updates.sh
+++ config/waybar/scripts/updates.sh
@@ -36,10 +36,10 @@ while true; do
     done
 
     # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
+    # and network uptime, only checking once every 1 hour for new updates
     while (( UPDATES == 0 )); do
         echo "None"
-        sleep 1800
+        sleep 3600
         get_total_updates
     done
 done
