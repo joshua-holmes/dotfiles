8a04e4cfa1d276d7c397209fc6240e72b60f6cfa
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-07-29 09:17:44 -0700

    Customize options

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -2,12 +2,15 @@
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
-vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.completeopt = {                         -- mostly just for cmp
+    "menuone", "noselect", "preview", "noinsert"
+}
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.history = 1000                          -- how many ":" commands and search patterns to remember
 vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true                       -- ignore case in search patterns
-vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim MOUSE
 vim.opt.pumheight = 10                          -- pop up menu height
 vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
 vim.opt.showtabline = 2                         -- always show tabs
@@ -20,22 +23,21 @@ vim.opt.termguicolors = true                    -- set term gui colors (most ter
 vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                         -- enable persistent undo
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.writebackup = true                      -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true                        -- convert tabs to spaces
-vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4                             -- insert 4 spaces for a tab
 vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.number = true                           -- set numbered lines
-vim.opt.relativenumber = false                  -- set relative numbered lines
-vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
+vim.opt.relativenumber = true                   -- set relative numbered lines
+vim.opt.numberwidth = 4                         -- set number column width to 4
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- is one of my fav
-vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.wrap = true                             -- wrap lines of text when they are too long
+vim.opt.scrolloff = 999                         -- number of lines to keep above and below cursor
+vim.opt.sidescrolloff = 8                       -- number of lines to keep to the left and right of the cursor
+vim.opt.whichwrap = "bs<>[]hl"                  -- which "horizontal" keys are allowed to travel to prev/next line
 
-vim.opt.shortmess:append "c"
+vim.opt.iskeyword:append "-"
+vim.opt.formatoptions:append({ "r", "n" })
+vim.opt.formatoptions:remove({ "o", "2" })
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
