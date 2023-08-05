719b8e9780c9e34f55f8842d8cb37d0bc391c10a
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-05 00:44:52 -0700

    Format

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -57,4 +57,3 @@ keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -4,26 +4,26 @@ if not status_ok then
     return
 end
 
-  configs.setup {
+configs.setup {
     -- ensure_installed = { "all" }, -- put the language you want in this array
     ensure_installed = "all", -- one of "all" or a list of languages
     -- ignore_install = { },                                                       -- List of parsers to ignore installing
-    sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,     -- install languages synchronously (only applied to `ensure_installed`)
 
     highlight = {
-      enable = true,       -- false will disable the whole extension
-      -- disable = { }, -- list of language that will be disabled
+        enable = true, -- false will disable the whole extension
+        -- disable = { }, -- list of language that will be disabled
     },
     autopairs = {
-      enable = true,
+        enable = true,
     },
     indent = {
-      enable = true,
-      -- disable = { "python", "css" }
+        enable = true,
+        -- disable = { "python", "css" }
     },
 
     context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
+        enable = true,
+        enable_autocmd = false,
     },
-  }
+}
