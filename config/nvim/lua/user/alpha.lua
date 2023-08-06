a914e7c73c38a01d69165b3f217aa50de185ce19
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-05 22:49:58 -0700

    Customize alpha menu, including random quotes

--- config/nvim/lua/user/alpha.lua
+++ config/nvim/lua/user/alpha.lua
@@ -15,21 +15,23 @@ dashboard.section.header.val = {
 }
 dashboard.section.buttons.val = {
     dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
     dashboard.button("t", "󱎸  Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
     dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
     dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
     -- NOTE: requires the fortune-mod package to work
-    -- local handle = io.popen("fortune")
-    -- local fortune = handle:read("*a")
-    -- handle:close()
-    -- return fortune
-    return "chrisatmachine.com"
+    local handle = io.popen("fortune")
+    local fortune = "jpholmes.com" -- default if fortune-mod doesn't owrk
+    if handle then
+        fortune = handle:read("*a")
+        handle:close()
+    end
+    return fortune
 end
 
 dashboard.section.footer.val = footer()
@@ -39,5 +41,4 @@ dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
