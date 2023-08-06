a0ef70fdd60ef76cefd88850527ac976f1e9f7db
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-05 20:19:44 -0700

    Tweak nvim-tree keybinds and fix filter to show .gitignore items

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -1,14 +1,8 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
+    print("nvim-tree could not be loaded")
     return
 end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-    return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 -- disable netrw, since it interferes with nvim-tree and nvim-tree replaces it
 vim.g.loaded_netrw = 1
@@ -60,15 +54,8 @@ nvim_tree.setup {
     view = {
         width = 30,
         side = "left",
-        mappings = {
-            list = {
-                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-                { key = "h",                  cb = tree_cb "close_node" },
-                { key = "v",                  cb = tree_cb "vsplit" },
-            },
-        },
     },
-    filter = {
+    filters = {
         git_ignored = false,
     },
 }
