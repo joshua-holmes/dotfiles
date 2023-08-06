6440e696458b612b8d420664b428d32088f891f9
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-05 20:17:37 -0700

    Add additional key to delete buffers

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -35,6 +35,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-c>", "<cmd>Bdelete!<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", ":m .+1<CR>==", opts)
