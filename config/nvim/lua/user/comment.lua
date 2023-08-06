478f0723f3808379590739614716ae8ba2d7510e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-05 20:13:25 -0700

    Update comment.nvim prehook to match modern instructions

--- config/nvim/lua/user/comment.lua
+++ config/nvim/lua/user/comment.lua
@@ -4,29 +4,13 @@ if not status_ok then
 end
 
 comment.setup {
-    pre_hook = function(ctx)
-        local U = require "Comment.utils"
-
-        local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
-        if not status_utils_ok then
+    pre_hook = function()
+        local status_ok_ts, ts_integrations = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+        if not status_ok_ts then
+            print("Cannot load ts_context_commentstring integration")
             return
         end
 
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-            location = utils.get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = utils.get_visual_start_location()
-        end
-
-        local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
-        if not status_internals_ok then
-            return
-        end
-
-        return internals.calculate_commentstring {
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-        }
+        ts_integrations.create_pre_hook()
     end,
 }
