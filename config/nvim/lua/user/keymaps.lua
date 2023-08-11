96b8f2cfb0dd80c69a74a90d9f9205a58d6f09c0
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-11 14:10:18 -0700

    Try keybind for vertical and horizontal window splitting

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -25,6 +25,8 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<C-s>", ":write<CR>", opts)
+keymap("n", "<C-x>", ":split<CR>", opts)
+keymap("n", "<C-c>", ":vsplit<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
