fa53d6aa9790295bb4735b596e554184caf5aa9c
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-29 20:33:33 -0700

    Create script for retrieving wallpaper remotely

--- config/swww/scripts/set-random-image.sh
+++ config/swww/scripts/set-random-image.sh
@@ -10,22 +10,34 @@ fi
 
 source "$(dirname $0)/../swww.conf" 
 
-current_image=$(realpath $WP_IMAGES/$(swww query\
-    | grep --only-matching --extended-regexp 'image: ".+"'\
-    | cut --delimiter \" --fields 2\
-    | uniq\
-    ))
+get_local_img() {
+    current_image=$(realpath $WP_IMAGES/$(swww query\
+        | grep --only-matching --extended-regexp 'image: ".+"'\
+        | cut --delimiter \" --fields 2\
+        | uniq\
+        ))
 
-new_image=$(find "$WP_IMAGES" \
-    | while read -r img; do
-        if [[ "$img" != "$WP_IMAGES" && "$img" != "$current_image" ]]; then
-           echo "$((RANDOM % 1000)):$img"
-        fi
-    done \
-    | sort --numeric-sort\
-    | grep --max-count 1 ""\
-    | cut --delimiter ':' --fields 2-\
-    )
+    new_image=$(find "$WP_IMAGES" \
+        | while read -r img; do
+            if [[ "$img" != "$WP_IMAGES" && "$img" != "$current_image" ]]; then
+               echo "$((RANDOM % 1000)):$img"
+            fi
+        done \
+        | sort --numeric-sort\
+        | grep --max-count 1 ""\
+        | cut --delimiter ':' --fields 2-\
+        )
+}
+
+get_remote_img() {
+    IMG_CACHE="${HOME}/.cache/swww/img_download"
+    IMG_URL="https://picsum.photos/1920/1080"
+    wget -qO "$IMG_CACHE" "$IMG_URL"
+    new_image="$IMG_CACHE"
+}
+
+# get_local_img
+get_remote_img
 
 swww img\
     --transition-type "$TYPE"\
