415b857b9debe3420a024940197346005b9e4258
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-09-25 17:02:49 -0700

    Add ability to cleanup include headers in c and c++ files only

--- config/nvim/lua/user/whichkey.lua
+++ config/nvim/lua/user/whichkey.lua
@@ -4,24 +4,41 @@ if not status_ok then
     return
 end
 
+vim.cmd(([[
+autocmd FileType cpp lua C_INCLUDE_HEADER_CLEANUP()
+autocmd FileType c lua C_INCLUDE_HEADER_CLEANUP()
+]]))
+
+function C_INCLUDE_HEADER_CLEANUP()
+    which_key.register({
+        ["<leader>"] = {
+            l = {
+                c = {
+                    "<cmd>!clang-include-cleaner --edit " .. vim.api.nvim_buf_get_name(0) .. "<cr>", 'C++ "include" header cleanup'
+                }
+            }
+        }
+    })
+end
+
 local setup = {
     plugins = {
-        marks = true,   -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        marks = true,         -- shows a list of your marks on ' and `
+        registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
-            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
             suggestions = 30, -- how many suggestions should be shown in the list?
         },
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-            motions = true, -- adds help for motions
+            operators = false,   -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            motions = true,      -- adds help for motions
             text_objects = true, -- help for text objects triggered after entering an operator
-            windows = true, -- default bindings on <c-w>
-            nav = true,    -- misc bindings to work with windows
-            z = true,      -- bindings for folds, spelling and others prefixed with z
-            g = true,      -- bindings for prefixed with g
+            windows = true,      -- default bindings on <c-w>
+            nav = true,          -- misc bindings to work with windows
+            z = true,            -- bindings for folds, spelling and others prefixed with z
+            g = true,            -- bindings for prefixed with g
         },
     },
     -- add operators that will trigger motion and text object completion
@@ -41,25 +58,25 @@ local setup = {
     },
     popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
     },
     window = {
-        border = "rounded",   -- none, single, double, shadow
-        position = "bottom",  -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "rounded",       -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
     },
     layout = {
-        height = { min = 4, max = 25 },                                         -- min and max height of the columns
-        width = { min = 20, max = 50 },                                         -- min and max width of the columns
-        spacing = 3,                                                            -- spacing between columns
-        align = "left",                                                         -- align columns left, center or right
+        height = { min = 4, max = 25 },                                           -- min and max height of the columns
+        width = { min = 20, max = 50 },                                           -- min and max width of the columns
+        spacing = 3,                                                              -- spacing between columns
+        align = "left",                                                           -- align columns left, center or right
     },
-    ignore_missing = false,                                                     -- enable this to hide mappings for which you didn't specify a label
+    ignore_missing = false,                                                       -- enable this to hide mappings for which you didn't specify a label
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-    show_help = true,                                                           -- show help message on the command line when the popup is visible
-    triggers = "auto",                                                          -- automatically setup triggers
+    show_help = true,                                                             -- show help message on the command line when the popup is visible
+    triggers = "auto",                                                            -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specify a list manually
     triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
@@ -72,7 +89,7 @@ local setup = {
 
 local default_opts = {
     prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
     silent = true, -- use `silent` when creating keymaps
     nowait = true, -- use `nowait` when creating keymaps
 }
@@ -120,7 +137,7 @@ local n_mappings = {
         s = { "<cmd>PackerSync<cr>", "Sync" },
         S = { "<cmd>PackerStatus<cr>", "Status" },
         u = { "<cmd>PackerUpdate<cr>", "Update" },
-   },
+    },
 
     g = {
         name = "Git",
@@ -195,6 +212,13 @@ local n_mappings = {
         v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
     },
 }
+local stuff = vim.bo.filetype
+print("STUFF", stuff)
+-- if vim.lsp.get_active_clients() then
+--     n_mappings.l.c = {
+--         "<cmd>!clang-include-cleaner --edit " .. vim.api.nvim_buf_get_name(0) .. "<cr>", 'C++ "include" header cleanup'
+--     }
+-- end
 
 local n_mappings_remap = {
     ["/"] = { "gcc", "Comment" },
