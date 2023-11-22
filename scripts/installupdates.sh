a4b6bef59341a01dadc65987542aa61d84cca2d7
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-11-22 07:07:17 -0800

    Remove grub config since I don't use grub

--- scripts/installupdates.sh
+++ scripts/installupdates.sh
@@ -61,7 +61,6 @@ then
                 read -p "Enter a comment for the snapshot: " c
                 sudo timeshift --create --comments "$c"
                 sudo timeshift --list
-                sudo grub-mkconfig -o /boot/grub/grub.cfg
                 echo "DONE. Snapshot $c created!"
                 echo ""
             break;;
