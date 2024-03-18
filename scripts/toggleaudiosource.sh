03eeea3cc6738217d21677a2d6a785ce6e3192bb
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-03-18 14:21:20 -0700

    Update speaker name

--- scripts/toggleaudiosource.sh
+++ scripts/toggleaudiosource.sh
@@ -1,5 +1,5 @@
 hyprx="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00.analog-stereo"
-external_speaker="alsa_output.pci-0000_0a_00.4.analog-stereo"
+external_speaker="alsa_output.pci-0000_0b_00.4.analog-stereo"
 
 if [[ $(pactl get-default-sink) = "${hyprx}" ]]; then
     pactl set-default-sink "${external_speaker}"
