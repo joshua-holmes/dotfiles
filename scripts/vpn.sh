a3fb2462fe86003707d7c274150d16ba68f5ba98
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-04-19 14:33:29 -0700

    Improve vpn usage script

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 if [[ $(openvpn3 sessions-list) = "No sessions available" ]]; then
-    openvpn3 session-start --config ~/vpn/josh.ovpn
+    if [[ -z $1 ]]; then
+        echo -e "Config name was not provided. See below for a list of available config names that you can use. To disconnect after connecting, just type \"vpn\"\n"
+        echo "EXAMPLE"
+        echo -e "\t$0 <config-name>\n"
+        openvpn3 configs-list
+        exit
+    fi
+    openvpn3 session-start --config "$1"
 else
-    openvpn3 session-manage --disconnect --config ~/vpn/josh.ovpn
+    active_sessions="$(openvpn3 sessions-list | rg -i 'Config name' | cut -d':' -f2 | xargs)"
+    for session in $active_sessions; do
+        echo "Disconnecting from session: ${session}"
+        openvpn3 session-manage --disconnect --config "${session}"
+    done
 fi
