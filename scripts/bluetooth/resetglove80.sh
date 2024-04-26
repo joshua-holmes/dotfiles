7a3542a77553bb840d7a8575953c9bad25f590c6
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-04-26 07:56:12 -0700

    Move bluetooth scripts and fix them by adding a python agent

--- scripts/bluetooth/resetglove80.sh
+++ scripts/bluetooth/resetglove80.sh
@@ -2,9 +2,17 @@
 
 glove80_id='F0:B9:FA:39:A7:C0'
 timeout=1
+d="$(realpath $(dirname $0))"
+
+# start agent using python
+python "${d}/bluez_simple_agent.py" &
+pid="$(jobs -l | cut -d' ' -f2 | xargs)"
+
+# remove old connection
 bluetoothctl disconnect "${glove80_id}"
 bluetoothctl remove "${glove80_id}"
 
+# scan for new connection
 for ((i = 0; i < 3; i++)); do
     bluetoothctl --timeout "$((timeout + i))" scan on
     if [[ "$(bluetoothctl devices)" = *"${glove80_id}"* ]]; then
@@ -12,5 +20,10 @@ for ((i = 0; i < 3; i++)); do
     fi
 done
 
+# connect to glove
+bluetoothctl agent on
 bluetoothctl pair "${glove80_id}"
 bluetoothctl connect "${glove80_id}"
+
+# kill old agent
+kill "$pid"
