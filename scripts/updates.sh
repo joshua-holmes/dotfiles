96a5cfffb9383eb76b36a5012a212b8f3b04ba2e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-10-20 00:07:27 -0700

    Use yay, not trizen

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -26,7 +26,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+if ! updates_aur=$(yay -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
