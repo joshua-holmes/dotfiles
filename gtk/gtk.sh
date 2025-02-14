5569de6a6242a8d14361f5b1dc3d0bf4077b3abe
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-02-13 21:34:29 -0800

    Update GTK

--- gtk/gtk.sh
+++ gtk/gtk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gnome_schema="org.gnome.desktop.interface"
-gsettings set "$gnome_schema" icon-theme "Papirus-Dark"
-gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Ice"
+gsettings set "$gnome_schema" icon-theme "Tela-circle-dark"
+gsettings set "$gnome_schema" cursor-theme "Graphite-dark-nord-cursors"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
 gsettings set "$gnome_schema" color-scheme "prefer-dark"
