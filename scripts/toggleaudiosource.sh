bca2efcd309235514c5d70d4538caa100bb22c94
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-03-04 16:05:14 -0800

    Switch name of hyprx for some reason

--- scripts/toggleaudiosource.sh
+++ scripts/toggleaudiosource.sh
@@ -1,4 +1,4 @@
-hyprx="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00.analog-stereo"
+hyprx="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00.stereo-fallback"
 external_speaker="alsa_output.pci-0000_0b_00.4.analog-stereo"
 
 if [[ $(pactl get-default-sink) = "${hyprx}" ]]; then
