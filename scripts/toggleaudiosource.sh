1bebf62bb90b96121bf9600039c299e118609361
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-03-18 18:36:39 -0700

    Get hyprx name programmatically

--- scripts/toggleaudiosource.sh
+++ scripts/toggleaudiosource.sh
@@ -1,4 +1,7 @@
-hyprx="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00.analog-stereo"
+# the name of this changes sometimes, so we get it programmatically
+hyprx_prefix="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00"
+hyprx=$(pactl list short sinks | rg ${hyprx_prefix} | cut -f2)
+
 external_speaker="alsa_output.pci-0000_0b_00.4.analog-stereo"
 
 if [[ $(pactl get-default-sink) = "${hyprx}" ]]; then
