8c39372df3c251aa91c56bda279b283ae6560ebd
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-06-26 23:45:53 +0000

    Remove GTK stuff

--- gtk/gtk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gnome_schema="org.gnome.desktop.interface"
-gsettings set "$gnome_schema" icon-theme "Tela-circle-dark"
-gsettings set "$gnome_schema" cursor-theme "Graphite-dark-nord-cursors"
-gsettings set "$gnome_schema" font-name "Cantarell 11"
-gsettings set "$gnome_schema" color-scheme "prefer-dark"
