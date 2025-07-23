1c66c4e530fd02b5f30f48dbfac3ad9d1af270a3
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-07-23 00:08:40 -0700

    Add update button back to waybar, but with my own checkupdates script

---
+++ waybar/scripts/checkupdates.sh
@@ -0,0 +1,55 @@
+#   ____ _               _      _   _           _       _            
+#  / ___| |__   ___  ___| | __ | | | |_ __   __| | __ _| |_ ___  ___ 
+# | |   | '_ \ / _ \/ __| |/ / | | | | '_ \ / _` |/ _` | __/ _ \/ __|
+# | |___| | | |  __/ (__|   <  | |_| | |_) | (_| | (_| | ||  __/\__ \
+#  \____|_| |_|\___|\___|_|\_\  \___/| .__/ \__,_|\__,_|\__\___||___/
+#                                    |_|                             
+
+cache=~/.cache/checkupdates
+cache_timeout_sec=3600
+signal=8
+
+checkupdates() {
+  paru -Qu | wc -l
+}
+
+get_cache_line() {
+  awk "NR==${1} {print; exit}" "${cache}"
+}
+
+if [[ "${1}" = "--help" || "${1}" = "-h" ]]; then
+  echo "--help, -h   | This help menu"
+  echo "--update, -u | Begin update process instead of checking for updates"
+  exit
+fi
+
+if [[ "${1}" = "--update" || "${1}" = "-u" ]]; then
+  ghostty -e "paru -Syu && rm ${cache}; echo Done - Press enter to exit; read"
+  pkill "-SIGRTMIN+${signal}" waybar
+  exit
+fi
+
+valid_cache="true"
+if [[ -f "${cache}" ]]; then
+  for line in $(cat "${cache}"); do
+    if [[ ! "${line}" =~ ^[0-9]+$ ]]; then
+      valid_cache="false"
+    fi
+  done
+  if (( "$(cat "${cache}" | wc -l)" < 2 )); then
+    valid_cache="false"
+  fi
+  if [[ "${valid_cache}" = "true" && $(( $(get_cache_line 1) + ${cache_timeout_sec} )) -lt $(date +%s) ]]; then
+    valid_cache="false"
+  fi
+else
+  valid_cache="false"
+fi
+
+if [[ "${valid_cache}" = "true" ]]; then
+  echo "$(get_cache_line 2)"
+else
+  updates="$(checkupdates)"
+  printf "$(date +%s)\n${updates}\n" > "${cache}"
+  echo "${updates}"
+fi
