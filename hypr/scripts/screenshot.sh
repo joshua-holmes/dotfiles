51e3422fcea9964de6b2d8a4691068f154a5f31b
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2026-01-15 22:29:19 -0800

    Waiting sucks so don't do it

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -6,7 +6,7 @@
 # |____/ \___|_|  \___|\___|_| |_|___/_| |_|\___/ \__| 
 #                                                      
 #  
-# by Stephan Raabe (2023) 
+# by Stephan Raabe (2023) ... and Josh now :P
 # ----------------------------------------------------- 
 
 NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
@@ -15,19 +15,19 @@ option1="Selected area"
 option2="Fullscreen"
 options="${option1}\n${option2}"
 
-waitOption1="Instant"
-waitOption2="Wait 3 seconds"
-waitOptions="${waitOption1}\n${waitOption2}"
+# waitOption1="Instant"
+# waitOption2="Wait 3 seconds"
+# waitOptions="${waitOption1}\n${waitOption2}"
 
 choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Take Screenshot")
 
-waitChoice=$(echo -e "$waitOptions" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Wait interval")
+# waitChoice=$(echo -e "$waitOptions" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Wait interval")
 
-case $waitChoice in
-    $waitOption2)
-        sleep 3
-    ;;
-esac
+# case $waitChoice in
+#     $waitOption2)
+#         sleep 3
+#     ;;
+# esac
 
 case $choice in
     $option1)
